abc03dc1661b4731751ef28699026b8df9b752ab
Eitch1106 <garcia.irv@gmail.com>
2023-08-24 14:30:16 -0600

    Feat: complete show issues and add infiniteScroll

--- react-issues/src/api/ApiGithub.js
+++ react-issues/src/api/ApiGithub.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const GITHUB_API_URL = 'https://api.github.com';
-const ACCESS_TOKEN = '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';
+//se tuvo que usar aquí el token porque al crearlo en el .env no funcionaba por detalles de commpilación
+const ACCESS_TOKEN = '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';//agrega aquí tu token personal creado en GITHUB
 
 const axiosInstance = axios.create({
   baseURL: GITHUB_API_URL,
@@ -10,23 +11,19 @@ const axiosInstance = axios.create({
   },
 });
 
-export const searchBugs = async (keyword) => {
+export const searchBugs = async (keyword, pageNumber) => {
   try {
-    console.log("Keyword a buscar: ",keyword);
+    console.log("Keyword a buscar: ", keyword);
+    const perPage = 10;
     const responseIssues = await axiosInstance.get('/repos/facebook/react/issues', {
       params: {
         state: 'open',
         q: `is:issue is:open label:bug ${keyword}`,
+        per_page: perPage,
+        page: pageNumber,
       },
-    }
-    );
-    const formattedIssues = responseIssues.data.map((issue) => ({
-      id: issue.id,
-      title: issue.title,
-      url: issue.html_url,
-    }));
-
-    console.log(formattedIssues);
+    });
+    console.log(responseIssues.data);
     return responseIssues.data;
   } catch (error) {
     throw error;
--- react-issues/src/components/Results.js
+++ react-issues/src/components/Results.js
@@ -1,36 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import { searchBugs } from '../api/ApiGithub';
+import React, { useState, useEffect } from "react";
+import { searchBugs } from "../api/ApiGithub";
 
 const BugList = (props) => {
   const [bugs, setBugs] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const bugsData = await searchBugs(props.keyword);
-        setBugs(bugsData);
-        console.log(bugsData);
+        setLoading(true);
+        const bugsData = await searchBugs(props.keyword, pageNumber);
+        setBugs((prevBugs) => [...prevBugs, ...bugsData]);
+        setLoading(false);
       } catch (error) {
-        console.error('Error fetching bugs:', error);
+        console.error("Error fetching bugs:", error);
+        setLoading(false);
       }
     }
+
     fetchData();
-  }, [props.keyword]);
+  }, [props.keyword, pageNumber]);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (
+        window.innerHeight + document.documentElement.scrollTop >=
+        document.documentElement.scrollHeight - 100
+      ) {
+        if (!loading) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      }
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [loading]);
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4">React Bug List</h1>
+      <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4">
+        React Bug List
+      </h1>
       <ul className="bg-white rounded-lg shadow-md p-4">
-        {bugs.map(bug => (
-          <li key={bug.id} className="mb-2">
+        {bugs.map((bug,index) => (
+          <li key={`${bug.id}-${index}`} className="mb-4">
             <a
               href={bug.html_url}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
+              className="text-blue-500 hover:underline text-lg font-semibold"
             >
               {bug.title}
             </a>
+            {bug.labels.length > 0 && bug.labels[0].description && (
+              <p className="text-gray-600">{bug.labels[0].description}</p>
+            )}
+            <p className="text-sm text-gray-500">
+              Created by {bug.author} on {bug.created_at}
+            </p>
           </li>
         ))}
       </ul>
