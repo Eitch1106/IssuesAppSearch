b1f1081d15ec7e9968a991e4e864bb872b1101de
Eitch1106 <garcia.irv@gmail.com>
2023-08-23 20:36:05 -0600

    feat: add picoCSS and create components

---
+++ react-issues/src/components/Search.js
@@ -0,0 +1,18 @@
+function SearchBugs() {
+    return (
+        <div className="container">
+            <h1 className="text-center">Search Issues about React</h1>
+            <div className="grid">
+                <div>
+                    <input placeholder="Ingresa una palabra clave" />
+                    <button>Search</button>
+                </div>
+                <div>
+                    
+                </div>
+                
+            </div>
+        </div>
+    );
+}
+export default SearchBugs
